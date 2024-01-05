39ea3386bc32daea2c2b6e41d51ca4c48aef70f4
lazurs1 <lazurs1@gmail.com>
2024-01-05 17:16:53 -0500

    1
    1

---
+++ CIS211/ConsoleApplication1T/ConsoleApplication1T.cpp
@@ -0,0 +1,89 @@
+// ConsoleApplication1T.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+#include <iostream>
+using namespace std;
+    int main()
+{       
+        //2-3
+        //cout << "Programin is fun";
+        
+        //2-4
+        //cout << "Programing is fun " << "more fun";
+        //cout << "Programing is fun ";
+        //cout << "more fun";
+
+        //2-5
+        //cout << "The folliwing items were top sellers" << endl;
+        //cout << "during the month of June:" << endl;
+        //cout << "Computer games" << endl;
+        //cout << "Coffee" << endl;
+        //cout << "Asprin" << endl;
+
+        //2-6
+        //cout << "The following items were top sellers\n";
+        //cout << "during the month opf June\n";
+        //cout << "Computergames\nCoffee";
+        //cout << "\nAsprin\n";
+        
+        //2-7
+        //int number;
+        //number = 5;
+        //cout << "The value of the number is " << number << endl;
+
+        //number = 7;
+        //cout << "Now the value of the nmumber is " << number << endl;
+
+
+        ////2-9
+        //int checking;
+        //unsigned int miles;
+        //long diameter;
+
+        //checking = -20;
+        //miles = 4276;
+        //diameter = 100000;
+
+        //cout << "We have made a jorney of " << miles << " miles.\n";
+        //cout << "Our checking accounting balance is " << checking << " dollars\n";
+        //cout << "The mMilky way galaxy is about " << diameter;
+        //cout << " light years in diameter.\n";
+        
+        //2-10
+        //int floors = 56,
+        //    rooms = 300,
+        //    suites = 300;
+
+        //cout << "The Grande Hotel has " << floors << " floors\n";
+        //cout << "with " << rooms << " rooms and " << suites;
+        //cout << " suites.\n";
+
+        //2-11
+        float distance = 1.496E8;
+        double mass = 1.989E30;
+
+        cout << "The Sun is " << distance << " kilometers away.\n";
+        cout << "The Sun\'s mass is " << mass << " kilograms.\n";
+
+
+    return 0;
+}
