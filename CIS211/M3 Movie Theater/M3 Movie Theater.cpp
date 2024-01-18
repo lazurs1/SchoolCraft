02ae65876f938f09397c5e1ece6fda3b8bf0f740
lazurs1 <lazurs1@gmail.com>
2024-01-17 22:12:09 -0500

    1
    1

---
+++ CIS211/M3 Movie Theater/M3 Movie Theater.cpp
@@ -0,0 +1,19 @@
+// M3 Movie Theater.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+Prior to submitting this assignment, make sure you have completed the associated Learning Activities and Practices Exercises in the In this module you learned more detail about expressions and interactivity in
+C++ and how to tie these together with the material from previous modules to solve problems.
+Movie theaters often make more money on food and drink sales than they do on ticket sales. One particular theater charges $12 for tickets to evening shows and just $5 for kiddie matinees.
+They have discovered that the average evening patron spends $8.50 on concessions and the average matinee patron spends $10.50 on concessions.
+Write a program that computes and displays what percent of evening show income and what percent of matinee show income comes from ticket sales and what percent comes from concession stand purchases.
+Complete the C++ code using Visual Studio or Xcode, compress (zip) and upload the entire project folder to the submission area by the listed due date.
+This assignment is worth 20 points and will be evaluated using the CIS 211 Assignment Rubric.
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
