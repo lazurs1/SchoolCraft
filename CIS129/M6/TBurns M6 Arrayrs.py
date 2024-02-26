319b7891f4eb84b2324793f967e075fa030611f8
lazurs1 <lazurs1@gmail.com>
2024-02-26 11:28:02 -0500

    1
    1

--- CIS129/M6/TBurns M6 Arrayrs.py
+++ CIS129/M6/TBurns M6 Arrayrs.py
@@ -27,9 +27,9 @@ Make a working version of this program in Python.
 '''
 
 PWList=("123456","123456789","qwerty","password","12345","qwerty123","1q2w3e","12345678","111111","1234567890")
-print (PWList[1])
 print("Welcome to the password checker")
 passCheck=input("Please input your password for check: ")
 while passCheck in PWList:
-    passCheck=input("Your password is in the check list, please try a new one:")
+    passCheck=input("Your password is in the check list, please try a new one: ")
 print("Your password was not in the list")
+exit()
