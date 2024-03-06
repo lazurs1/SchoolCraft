3cf17e47e68c402a501089b146a40c7b7fac8038
lazurs1 <lazurs1@gmail.com>
2024-03-06 08:43:15 -0500

    1
    1

--- CIS211/ConsoleApplication1T/ConsoleApplication1T.cpp
+++ CIS211/ConsoleApplication1T/ConsoleApplication1T.cpp
@@ -304,21 +304,48 @@ using namespace std;
     //    cout << "False\n";
 
 
-    //file ops
-    ofstream outputFile;
-    //open file
-    outputFile.open("demofile.txt");
-    cout << "Now writing data to the file.\n";
-    outputFile << "Bach\n";
-    outputFile << "bet\n";
-    outputFile << "Motz\n";
-    outputFile << "sch\n";
-    outputFile << "Blnk182\n";
-    outputFile << "Othere\n";
+    ////file ops
+    //ofstream outputFile;
+    ////open file
+    //outputFile.open("demofile.txt");
+    //cout << "Now writing data to the file.\n";
+    //outputFile << "Bach\n";
+    //outputFile << "bet\n";
+    //outputFile << "Motz\n";
+    //outputFile << "sch\n";
+    //outputFile << "Blnk182\n";
+    //outputFile << "Othere\n";
 
-    //Close file
-    outputFile.close();
-    cout << "Done. \n";
+    ////Close file
+    //outputFile.close();
+    //cout << "Done. \n";
 
+
+    double x, y, quotient, total = 0.0;
+    for (x = 1, y = 30; x <= 30; x++, y--)
+    {   
+        quotient = x / y;
+        cout << "x=" << x << " y=" << y << " quot=" << quotient << "\n";
+        total += quotient;
+    }
+    cout << "total: " << total;
+
+
+    for (int dnom = 2; dnom <= 1024; dnom *= 2)
+    {
+        total = +1.0 / dnom;
+    }
+    cout << "\nTotal = " << total;
+
+
+    cout << "\n-----------------------------------------\n";
+    int zzz = 4;
+    int &i2 = zzz;
+    cout << zzz << " zzz\n";
+    cout << i2 << " i2\n";
+    i2 = 243234;
+    cout << "\n-----------------------------------------\n";
+    cout << zzz << " zzz\n";
+    cout << i2 << " i2\n";
 
 }
